af831ca1c227bede24ad9fb5171d2561b2c57085
Thor <thor@10.14.6>
2019-09-20 13:54:57 +0200

    fredag på klassen

--- NumberGuess/src/numberguess/KlasseUge38.java
+++
@@ -1,7 +0,0 @@
-package numberguess;
-public class KlasseUge38 {
-	public static void main(String[] main) {
-
-	}
-	
-}
--- NumberGuess/src/numberguess/OnStrings.java
+++
@@ -1,6 +0,0 @@
-package numberguess;
-public class OnStrings {
-	public static void main(String[] args) {
-		System.out.printf("Mit firma hedder: \u00A4 \u00A9 MyBW\n");
-	}
-}
